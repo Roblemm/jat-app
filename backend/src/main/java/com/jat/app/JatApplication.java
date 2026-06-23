10398fb286df2ce488c9c64d899f7f2e765f5efe
Jathniel Ahonsi <littleahonsi@gmail.com>
2026-06-22 20:49:20 -0400

    chore: scaffold spring boot backend

---
+++ backend/src/main/java/com/jat/app/JatApplication.java
@@ -0,0 +1,12 @@
+package com.jat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JatApplication.class, args);
+    }
+}
